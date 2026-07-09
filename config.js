efa192bf69e7ba700fe208599175d1932eb38c23
Miсhael Belvedersky <mike.belveder@gmail.com>
2026-07-09 05:12:43 +0300

    chore: адрес туннеля

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.KOTODOM_API = "https://separation-finally-allowing-elimination.trycloudflare.com";
+window.KOTODOM_API = "https://childrens-roulette-interpretation-organisations.trycloudflare.com";
